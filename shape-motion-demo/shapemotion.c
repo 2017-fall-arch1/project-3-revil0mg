1df5577ff2198a8789632a1f685799ba62f42832
Oliver Martinez <omartinez14@miners.utep.edu>
2017-12-05 08:00:00 -0600

    added assembly method

--- shape-motion-demo/shapemotion.c
+++ shape-motion-demo/shapemotion.c
@@ -13,33 +13,38 @@
 #include <p2switches.h>
 #include <shape.h>
 #include <abCircle.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <buzzer.h>
 
 #define GREEN_LED BIT6
 
 
-AbRect rect10 = {abRectGetBounds, abRectCheck, {10,10}}; /**< 10x10 rectangle */
-AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
+AbRect rect10 = {abRectGetBounds, abRectCheck, {10,2}}; /**< 10x10 rectangle */
+AbRect rect14 = {abRectGetBounds, abRectCheck, {10,2}}; /**< 10x10 rectangle */
+//AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
 
 AbRectOutline fieldOutline = {	/* playing field */
   abRectOutlineGetBounds, abRectOutlineCheck,   
   {screenWidth/2 - 10, screenHeight/2 - 10}
 };
 
+/*
 Layer layer4 = {
   (AbShape *)&rightArrow,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
+  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center *//*
+  {0,0}, {0,0},				    /* last & next pos */ /*
   COLOR_PINK,
   0
 };
-  
+*/
 
 Layer layer3 = {		/**< Layer with an orange circle */
   (AbShape *)&circle8,
   {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_VIOLET,
-  &layer4,
+  COLOR_WHITE,
+  0,
 };
 
 
@@ -53,15 +58,15 @@ Layer fieldLayer = {		/* playing field as a layer */
 
 Layer layer1 = {		/**< Layer with a red square */
   (AbShape *)&rect10,
-  {screenWidth/2, screenHeight/2}, /**< center */
+  {screenWidth/2, screenHeight/2 + 61}, /**< center */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_RED,
   &fieldLayer,
 };
 
 Layer layer0 = {		/**< Layer with an orange circle */
-  (AbShape *)&circle14,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
+  (AbShape *)&rect14,
+  {(screenWidth/2)+10, (screenHeight/2)-61}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_ORANGE,
   &layer1,
@@ -79,10 +84,15 @@ typedef struct MovLayer_s {
 
 /* initial value of {0,0} will be overwritten */
 MovLayer ml3 = { &layer3, {1,1}, 0 }; /**< not all layers move */
-MovLayer ml1 = { &layer1, {1,2}, &ml3 }; 
-MovLayer ml0 = { &layer0, {2,1}, &ml1 }; 
+MovLayer ml1 = { &layer1, {1,0}, &ml3 };
+MovLayer ml0 = { &layer0, {-1,0}, &ml1 }; 
 
-void movLayerDraw(MovLayer *movLayers, Layer *layers)
+
+volatile int p1Score = 0;
+volatile int p2Score = 0;
+
+
+movLayerDraw(MovLayer *movLayers, Layer *layers)
 {
   int row, col;
   MovLayer *movLayer;
@@ -122,12 +132,39 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
 
 
 //Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
-
+Region paddle1;
+Region paddle2;
 /** Advances a moving shape within a fence
  *  
  *  \param ml The moving shape to be advanced
  *  \param fence The region which will serve as a boundary for ml
  */
+
+void mlBounce(MovLayer *ml, Region *botPaddle, Region *topPaddle)
+{
+  Vec2 newPos;
+  u_char axis;
+  Region shapeBoundary;
+  for (; ml; ml = ml->next) {
+    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+    
+    if (((shapeBoundary.botRight.axes[0] >= topPaddle->botRight.axes[0]) &&
+      (shapeBoundary.botRight.axes[1] > topPaddle->topLeft.axes[1]) &&
+      (shapeBoundary.botRight.axes[1] < topPaddle->botRight.axes[1])) ||
+      ((shapeBoundary.topLeft.axes[0] <= botPaddle->topLeft.axes[0]) &&
+      (shapeBoundary.topLeft.axes[1] > botPaddle->topLeft.axes[1]) &&
+      (shapeBoundary.topLeft.axes[1] < botPaddle->botRight.axes[1]))) {
+
+        int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+        newPos.axes[axis] += (2*velocity);
+    
+    } /**< for axis */
+    ml->layer->posNext = newPos;
+  } /**< for ml */
+}
+
+
 void mlAdvance(MovLayer *ml, Region *fence)
 {
   Vec2 newPos;
@@ -142,10 +179,48 @@ void mlAdvance(MovLayer *ml, Region *fence)
 	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
 	newPos.axes[axis] += (2*velocity);
       }	/**< if outside of fence */
+      
+      if (shapeBoundary.topLeft.axes[1] < fence->topLeft.axes[0] ) {
+	p1Score++;
+        buzzer_high_pitch();
+      }	
+
+      if (shapeBoundary.botRight.axes[1] > fence->botRight.axes[1] ) {
+	p2Score++;
+        buzzer_high_pitch();
+      }	
     } /**< for axis */
     ml->layer->posNext = newPos;
   } /**< for ml */
 }
+
+
+
+
+/* adapted from http://stackoverflow.com/questions/9655202/how-to-convert-integer-to-string-in-c
+ * Converts int to string 
+*/
+char* itoa(int i, char b[]){
+    char const digit[] = "0123456789";
+    char* p = b;
+    if(i<0){
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do{ //Move to where representation ends
+        ++p;
+        shifter = shifter/10;
+    }while(shifter);
+    *p = '\0';
+    do{ //Move back, inserting digits as u go
+        *--p = digit[i%10];
+        i = i/10;
+    }while(i);
+    return b;
+}
+
+
 
 
 u_int bgColor = COLOR_BLUE;     /**< The background color */
@@ -165,21 +240,28 @@ void main()
   configureClocks();
   lcd_init();
   shapeInit();
-  p2sw_init(1);
+
 
   shapeInit();
 
   layerInit(&layer0);
   layerDraw(&layer0);
-
+  
+  buzzer_init();
 
   layerGetBounds(&fieldLayer, &fieldFence);
 
+  p2sw_init(15);
+  or_sr(0x8);
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
 
 
+  /** Game logic **/
+
+  
   for(;;) { 
     while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
       P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
@@ -188,6 +270,37 @@ void main()
     P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
     redrawScreen = 0;
     movLayerDraw(&ml0, &layer0);
+
+    buzzer_set_period(0);
+
+    char p1ScoreString [2];
+    char p2ScoreString [2];
+    itoa(p1Score, p1ScoreString);
+    itoa(p2Score, p2ScoreString);
+    drawString5x7(1,1, "P2 Score:", COLOR_GREEN, COLOR_BLUE);
+    drawString5x7(1,152, "P1 Score:", COLOR_GREEN, COLOR_BLUE);
+    drawString5x7(70,152, p1ScoreString, COLOR_GREEN, COLOR_BLUE);
+    drawString5x7(70,1, p2ScoreString, COLOR_GREEN, COLOR_BLUE);
+    
+
+    u_int switches = p2sw_read(), i;
+    char str[5];
+    for (i = 0; i < 4; i++)
+      str[i] = (switches & (1<<i)) ? '0' : '1';
+    str[4] = 0;
+    if (str[0] == '0') {
+      ml1.velocity.axes[0] = -7;
+    }
+    if (str[1] == '1') {
+      ml1.velocity.axes[0] = 7; 
+    }
+    if (str[2] == '2') {
+      ml0.velocity.axes[0] = -7;
+    }
+    if (str[3] == '3') {
+      ml0.velocity.axes[0] = 7;
+    }
+
   }
 }
 
@@ -198,6 +311,9 @@ void wdt_c_handler()
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
   if (count == 15) {
+    layerGetBounds(&layer0, &paddle1);
+    layerGetBounds(&layer1, &paddle2);
+    mlBounce(&ml3, &paddle1, &paddle2);
     mlAdvance(&ml0, &fieldFence);
     if (p2sw_read())
       redrawScreen = 1;
